1f6aae58396d279cbea415673d7c2ec415a91caa
lYC92 <liyangchuan654@gmail.com>
2018-12-29 21:35:43 +0100

    waypoint update 50

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -52,7 +52,7 @@ class WaypointUpdater(object):
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(25)
+        rate = rospy.Rate(50)
         while not rospy.is_shutdown():
             if self.pose and self.base_lane:
                 self.publish_waypoints()
