d13f69e19719c951579d9755d9f0974c2fd2dfdd
lYC92 <liyangchuan654@gmail.com>
2018-12-31 23:33:52 +0100

    add tensorflow

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,7 +1,7 @@
 from styx_msgs.msg import TrafficLight
-# import tensorflow as tf
-# import numpy as np
-# import datetime
+import tensorflow as tf
+import numpy as np
+import datetime
 
 class TLClassifier(object):
     def __init__(self):
